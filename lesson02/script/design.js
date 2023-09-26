5c59c29d26850a90ab9356910aaaae7a8f20d591
thompsontst <thompsonabove@gmail.com>
2023-09-26 11:05:42 +0100

    restoration

---
+++ lesson02/script/design.js
@@ -0,0 +1,4 @@
+// copyright year
+document.querySelector('#year').textContent = new Date().getFullYear();
+var date = document.lastModified;
+                document.getElementById("lastModified").innerHTML = "Last Date Modified : " + date;
