dc1c6d7e787d73685be51e4bb6b645f6b5f8b7c0
thompsontst <thompsonabove@gmail.com>
2023-09-27 05:50:33 +0100

    menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,11 @@
 // copyright year
 document.querySelector('#year').textContent = new Date().getFullYear();
 var date = document.lastModified;
-                document.getElementById("lastModified").innerHTML = "Last Date Modified : " + date;
+    document.getElementById("lastModified").innerHTML = "Last Date Modified : " + date;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
