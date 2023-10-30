2772815b0bbdd7da2ec18d3ed623536a28876dd3
thompsontst <thompsonabove@gmail.com>
2023-10-30 23:21:34 +0100

    boom

--- lesson05/script/bom.js
+++ lesson05/script/bom.js
@@ -77,9 +77,3 @@ document.addEventListener("DOMContentLoaded", () => {
         input.focus();
     });
 });
-
-
-// copyright year
-document.querySelector('#year').textContent = new Date().getFullYear();
-var date = document.lastModified;
-    document.getElementById("lastModified").innerHTML = "Last Date Modified : " + date;
---
+++ lesson05/script/getdates.js
@@ -0,0 +1,5 @@
+// copyright year
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
